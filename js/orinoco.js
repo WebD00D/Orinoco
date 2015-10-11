7240ed14ca9c422b574e2ea985070a53b28a8c5c
Christian <Bryant>
2015-10-10 21:01:49 -0400

    added animation functionality to scroll down icon on home page

--- js/orinoco.js
+++ js/orinoco.js
@@ -1,8 +1,21 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function animate() {
+      $('.mainicon').animate({marginTop:'15px'}, 'slow', function(){
+          $('.mainicon').animate({marginTop:'0px'}, 'slow', function(){
+            animate();
+          });
+      });
+  }
+ animate();
+
+  $("#scrollDownIcon").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#allTheContent").offset().top
+    }, 700);
+  })
